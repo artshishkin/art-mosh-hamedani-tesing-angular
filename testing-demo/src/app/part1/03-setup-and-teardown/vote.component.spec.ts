7059aa03cf49cf98e28047befc04524459552359
Art <d.art.shishkin@gmail.com>
2021-09-13 16:29:22 +0300

    6.2. Setup and Tear Down - test (#1)

--- testing-demo/src/app/part1/03-setup-and-teardown/vote.component.spec.ts
+++ testing-demo/src/app/part1/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,31 @@
-import { VoteComponent } from './vote.component';
+import {VoteComponent} from './vote.component';
 
 describe('VoteComponent', () => {
-    it('', () => {
-    });
 
-    it('', () => {
-    });
+  let component: VoteComponent;
+  beforeEach(() => {
+    // Arrange
+    component = new VoteComponent();
+  });
+
+  afterEach(() => {
+  });
+
+  it('should increment totalVotes when upVoted', () => {
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+
+  });
+
+  it('should decrement totalVotes when downVoted', () => {
+
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
+
+  });
 });
