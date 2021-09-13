cd280489286949ba51b4f88174221fbfd04b358b
Art <d.art.shishkin@gmail.com>
2021-09-13 23:03:22 +0300

    10. Interaction Testing (#1)

--- testing-demo/src/app/part1/06-services/todos.component.spec.ts
+++ testing-demo/src/app/part1/06-services/todos.component.spec.ts
@@ -1,6 +1,6 @@
 import {TodosComponent} from "./todos.component";
 import {TodoService} from "./todo.service";
-import {from} from "rxjs";
+import {from, throwError} from "rxjs";
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -25,4 +25,50 @@ describe('TodosComponent', () => {
     expect(component.todos).toBe(todos);
 
   });
+
+  it('should call a server to save the changes when a new todo item is added', () => {
+
+    let spy = spyOn(service, "add").and.callFake((newTodo: any) => {
+      return from([true]);
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server (callFake)', () => {
+    let newTodoFromServer = {id: 1, title: 'Return from server'};
+    let spy = spyOn(service, "add").and.callFake((newTodo: any) => {
+      return from([newTodoFromServer]);
+    });
+
+    component.add();
+
+    console.log(component.todos);
+    expect(component.todos).toContain(newTodoFromServer);
+    expect(component.todos.indexOf(newTodoFromServer)).toBeGreaterThan(-1);
+  });
+
+  it('should add the new todo returned from the server (returnValue)', () => {
+    let newTodoFromServer = {id: 2, title: 'Return from server 2'};
+    let spy = spyOn(service, "add").and.returnValue(from([newTodoFromServer]));
+
+    component.add();
+
+    console.log(component.todos);
+    expect(component.todos).toContain(newTodoFromServer);
+    expect(component.todos.indexOf(newTodoFromServer)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property if server returns an error when adding a new todo', () => {
+
+    let errorMsg = "Can not add todo!";
+    let spy = spyOn(service, "add").and.returnValue(throwError(errorMsg));
+
+    component.add();
+
+    expect(component.message).toBe(errorMsg);
+  });
+
 });
