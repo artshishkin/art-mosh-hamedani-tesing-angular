74898c89c68d7e41979382883a18b644fd4014a3
Art <d.art.shishkin@gmail.com>
2021-09-14 21:36:00 +0300

    16.2. The Setup Code - TestBed (#3)

--- testing-demo/src/app/part2/1-voter/voter2.component.spec.ts
+++ testing-demo/src/app/part2/1-voter/voter2.component.spec.ts
@@ -1,10 +1,23 @@
 import {Voter2Component} from './voter2.component';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 describe('Voter2Component', () => {
 
-    beforeEach(() => {
+  let fixture: ComponentFixture<Voter2Component>;
+  let component: Voter2Component;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [Voter2Component]
     });
 
-    it('', () => {
-    });
+    fixture = TestBed.createComponent(Voter2Component);
+    component = fixture.componentInstance;
+    // fixture.nativeElement - root DOM element for this component template
+    // fixture.debugElement - wrapper arount native element
+
+  });
+
+  it('', () => {
+  });
 });
