1eafe7efea66a00416744cc13bd3f6ba26f33897
Art <d.art.shishkin@gmail.com>
2021-09-14 14:57:29 +0300

    14.3.1 VoterComponent - init (#2)

--- testing-demo/src/app/app.module.ts
+++ testing-demo/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import {TypedTextSummaryPipe} from './exercises/1- text-summary-pipe/typed-text-summary.pipe';
 import {TextSummaryPipe} from "./exercises/1- text-summary-pipe/text-summary.pipe";
 import {LikeComponent} from "./exercises/2- like-component/like.component";
+import {VoterComponent} from "./exercises/3- voter-component/voter.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     TextSummaryPipe,
     TypedTextSummaryPipe,
-    LikeComponent
+    LikeComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule
