ca9724d52ae6d6fed2877e8e749823f6f50b4434
Art <d.art.shishkin@gmail.com>
2021-09-14 22:11:00 +0300

    18. Testing Property and Class Bindings (#3)

--- testing-demo/src/app/part2/1-voter/voter2.component.spec.ts
+++ testing-demo/src/app/part2/1-voter/voter2.component.spec.ts
@@ -1,5 +1,6 @@
 import {Voter2Component} from './voter2.component';
 import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {By} from "@angular/platform-browser";
 
 describe('Voter2Component', () => {
 
@@ -14,10 +15,32 @@ describe('Voter2Component', () => {
     fixture = TestBed.createComponent(Voter2Component);
     component = fixture.componentInstance;
     // fixture.nativeElement - root DOM element for this component template
-    // fixture.debugElement - wrapper arount native element
+    // fixture.debugElement - wrapper around native element
 
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+
+    component.myVote = 1;
+    component.othersVote = 123;
+    fixture.detectChanges();
+
+    // fixture.debugElement.query(By.directive(Voter2Component))
+    let debugElement = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = debugElement.nativeElement;
+
+    expect(el.innerText).toContain('124');
   });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let debugElement = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(debugElement.classes['highlighted']).toBeTruthy();
+  });
+
+
 });
