9eb8556b1a053303d8c8d0b52c1e2b04a707f19f
Art <d.art.shishkin@gmail.com>
2021-09-16 13:47:56 +0300

    24.2. Dealing with Route Parameters - createSpyObj implementation (#3)

--- testing-demo/src/app/part2/3-user-details/user-details.component.stub.spec.ts
+++ testing-demo/src/app/part2/3-user-details/user-details.component.stub.spec.ts
@@ -27,22 +27,65 @@ describe('UserDetailsComponent using jasmine.createSpyObj', () => {
 
     routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
     activatedRouteSpy = TestBed.inject(ActivatedRoute) as jasmine.SpyObj<ActivatedRoute>;
-    activatedRouteSpy.params = from([]);
 
-    fixture = TestBed.createComponent(UserDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('and activated route without params', () => {
+    beforeEach(() => {
+
+      activatedRouteSpy.params = from([]);
+
+      fixture = TestBed.createComponent(UserDetailsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should redirect the customer to the users page after saving', () => {
+
+      component.save();
+
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['users']);
+    });
+
+
   });
 
-  it('should redirect the customer to the users page after saving', () => {
+  describe('and testing activated route with wrong params', () => {
 
-    component.save();
+    beforeEach(() => {
 
-    expect(routerSpy.navigate).toHaveBeenCalledWith(['users']);
+      activatedRouteSpy.params = from([{id: 0, name: 'wrong id'}]);
+
+      fixture = TestBed.createComponent(UserDetailsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should navigate the customer to the `not-found` page when NO valid user id is passed', () => {
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['not-found']);
+    });
+
+  });
+
+  describe('and testing activated route with correct params', () => {
+
+    beforeEach(() => {
+
+      activatedRouteSpy.params = from([{id: 123, name: 'User With Correct id'}]);
+
+      fixture = TestBed.createComponent(UserDetailsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should NOT navigate the customer to the `not-found` page when VALID user id is passed', () => {
+      expect(routerSpy.navigate).not.toHaveBeenCalled();
+    });
+
   });
 
 });
